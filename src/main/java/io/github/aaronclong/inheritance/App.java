7b73ca524b288f5e90b9ba21bf4c28ba0fbbaf06
Aaron Long <aaron.long.c@gmail.com>
2017-05-01 13:04:40 -0400

    Setup Project

---
+++ src/main/java/io/github/aaronclong/inheritance/App.java
@@ -0,0 +1,13 @@
+package io.github.aaronclong.inheritance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
